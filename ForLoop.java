8c1c4bb00f375e78b85625920a1a95b2e6575645
patilmanjunathp <98634302+patilmanjunathp@users.noreply.github.com>
2022-01-30 20:38:51 +0530

    Add files via upload
    Added ForLoop

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ForLoop {
+    public static void main(String[] args){
+        int i;
+        for(i=10;i>0;i--){
+            System.out.println(i);
+        }
+       // System.out.println(i);
+    }
+}
